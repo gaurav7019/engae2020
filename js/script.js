ca75c51d7df1f0f675b8a837b6c5b11dee9198b1
gaurav7019 <gaurav18e@iitg.ac.in>
2020-07-11 22:55:34 +0530

    Two Player Completed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,160 @@
 //Set Name Function
-function setName(){
-    let name1 = "Player 1";
-    let name2 = "Player 2";
-    let nameArr = document.querySelectorAll(".playerName");
+let name1;
+let name2;
+let nameArr = document.querySelectorAll(".playerName");
+let playerlabel = document.querySelector("#playerLabel");
+var p1 = document.getElementById("p1Name");
+var p2 = document.getElementById("p2Name");
+let back = document.querySelector("#backToMenu")
 
-    name1 = prompt("Enter the name of Player 1");
-    name2 = prompt("Enter the name of Palyer 2");
+let submit = document.querySelector("#submitButton");
+submit.addEventListener("click", startTwoPlayer);
 
-    nameArr[0].textContent = name1;
-    nameArr[1].textContent = name2;
+function startTwoPlayer(){
+  name1 = p1.value;
+  name2 = p2.value;
+  setName();
+  document.querySelector(".launchForm").classList.add("hideBoard");
+  document.querySelector(".board").classList.remove("hideBoard");
 }
 
-setName();
+function setName(){
+    if(!name1){
+      nameArr[0].textContent = "Player 1";
+    }
+    else{
+      nameArr[0].textContent = name1;
+    }
+
+    if(!name2){
+      nameArr[1].textContent = "Player 2"
+    }
+    else{
+      nameArr[1].textContent = name2;
+    }
+    playerlabel.textContent = name1;
+}
+
+back.addEventListener("click", ()=>{
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = "";
+    boxes[i].style.backgroundColor = "#dee9ec";
+    boxes[i].style.color = "black";
+  }
+  currentPlayer = "x";
+  name1 = "";
+  name2 = "";
+  p1.value = "";
+  p2.value = "";
+  nameArr[0].textContent = "";
+  nameArr[1].textContent = "";
+
+  document.querySelector(".launchForm").classList.remove("hideBoard");
+  document.querySelector(".board").classList.add("hideBoard");
+})
+
+//stores player turns
+let currentPlayer = "x";
+let currentPlayerName = name1;
+
+//stores the status of the game, whether its over or still in play
+let gameStatus = "Game On";
+
+//Gets all Boxes elements
+const boxes = document.getElementsByClassName("box");
+
+//loops through all the elements
+for (let i = 0; i < boxes.length; i++) {
+  //adds event listener to each box;
+  boxes[i].addEventListener("click", function() {
+    //checks if the box has an x or an o in it and also checks if the game is still on
+    if (boxes[i].innerHTML.trim() == "" && gameStatus == "Game On") {
+      //adds x or o for the current play in their choosen box
+      boxes[i].textContent = currentPlayer;
+
+      //changes player turns
+      currentPlayer = (currentPlayer == "x" ? "o" : "x");
+      currentPlayerName = (currentPlayer == "x" ? name1 : name2);
+
+      playerlabel.textContent = currentPlayerName;
+
+      //checks 3 matching x's or o's
+      if (
+        boxes[0].innerHTML == boxes[1].innerHTML &&
+        boxes[1].innerHTML == boxes[2].innerHTML &&
+        boxes[0].innerHTML.trim() != ""
+      ) {
+        showWinner(0, 1, 2);
+      } else if (
+        boxes[3].innerHTML == boxes[4].innerHTML &&
+        boxes[4].innerHTML == boxes[5].innerHTML &&
+        boxes[3].innerHTML.trim() != ""
+      ) {
+        showWinner(3, 4, 5);
+      } else if (
+        boxes[6].innerHTML == boxes[7].innerHTML &&
+        boxes[7].innerHTML == boxes[8].innerHTML &&
+        boxes[6].innerHTML.trim() != ""
+      ) {
+        showWinner(6, 7, 8);
+      } else if (
+        boxes[0].innerHTML == boxes[3].innerHTML &&
+        boxes[3].innerHTML == boxes[6].innerHTML &&
+        boxes[0].innerHTML.trim() != ""
+      ) {
+        showWinner(0, 3, 6);
+      } else if (
+        boxes[1].innerHTML == boxes[4].innerHTML &&
+        boxes[4].innerHTML == boxes[7].innerHTML &&
+        boxes[1].innerHTML.trim() != ""
+      ) {
+        showWinner(1, 4, 7);
+      } else if (
+        boxes[2].innerHTML == boxes[5].innerHTML &&
+        boxes[5].innerHTML == boxes[8].innerHTML &&
+        boxes[2].innerHTML.trim() != ""
+      ) {
+        showWinner(2, 5, 8);
+      } else if (
+        boxes[0].innerHTML == boxes[4].innerHTML &&
+        boxes[4].innerHTML == boxes[8].innerHTML &&
+        boxes[0].innerHTML.trim() != ""
+      ) {
+        showWinner(0, 4, 8);
+      } else if (
+        boxes[2].innerHTML == boxes[4].innerHTML &&
+        boxes[4].innerHTML == boxes[6].innerHTML &&
+        boxes[2].innerHTML.trim() != ""
+      ) {
+        showWinner(2, 4, 6);
+      }
+    }
+  });
+}
+
+//resets the game
+document.getElementById("reset").addEventListener("click", function() {
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = "";
+    boxes[i].style.backgroundColor = "#dee9ec";
+    boxes[i].style.color = "black";
+  }
+  currentPlayer = "x";
+  document.getElementById("message").style.display = "none";
+  document.getElementById("player").innerHTML = "X";
+  gameStatus = "Game On";
+});
+
+//displays the winner
+function showWinner(x, y, z) {
+  boxes[x].style.backgroundColor = "#0d8b70";
+  boxes[x].style.color = "white";
+  boxes[y].style.background = "#0d8b70";
+  boxes[y].style.color = "white";
+  boxes[z].style.background = "#0d8b70";
+  boxes[z].style.color = "white";
+  document.getElementById("winner").innerHTML =
+    currentPlayer == "x" ? "O" : "X";
+  document.getElementById("message").style.display = "block";
+  gameStatus = "Game Over";
+}
